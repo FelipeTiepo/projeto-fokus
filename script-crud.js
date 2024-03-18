ffe875c7b2112a8406a9214981543589996fa52a
Felipe Tiepo <ftiepo20@gmail.com>
2024-03-17 23:51:24 -0300

    adicionando o localStore

---
+++ script-crud.js
@@ -0,0 +1,20 @@
+// SELETORES
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+// VARIÁVEIS
+const tarefas = []
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
+})
